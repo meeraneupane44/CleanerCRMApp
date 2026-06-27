d24530f574f1f3d5607266ece30eb705253e172d
Meera <awnoop@gmail.com>
2026-06-27 15:12:43 -0500

    supabase update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 // /lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
-import 'react-native-url-polyfill/auto';
+import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://spvtvngfhkmwakjhqkgx.supabase.co";
+const supabaseAnonKey = "sb_publishable_wx3q7vgK8L9I80JxKiYsYA_voV2bM9j";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
